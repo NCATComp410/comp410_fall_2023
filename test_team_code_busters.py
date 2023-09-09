d51be64f6d7fcacb5b6af2c8372babd748dbc604
claesmk <claesmk@gmail.com>
2023-09-09 19:32:24 -0400

    Adding UT files for each team to help avoid merge conflicts

---
+++ test_team_code_busters.py
@@ -0,0 +1,8 @@
+import unittest
+from pii_scan import show_aggie_pride, analyze_text
+
+
+class TestTeamCodeBusters(unittest.TestCase):
+    def test_aggie_pride(self):
+        """Test to make sure the Aggie Pride function works"""
+        self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
---
+++ test_team_sci.py
@@ -0,0 +1,8 @@
+import unittest
+from pii_scan import show_aggie_pride, analyze_text
+
+
+class TestTeamSci(unittest.TestCase):
+    def test_aggie_pride(self):
+        """Test to make sure the Aggie Pride function works"""
+        self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
