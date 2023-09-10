a0ec1d4ff0efa714384b7178d3d1c25852bde658
mkc2mkc <66230745+mkc2mkc@users.noreply.github.com>
2023-09-10 15:53:19 +0000

    Added AU_MEDICARE test

--- test_team_null.py
+++ test_team_null.py
@@ -6,3 +6,10 @@ class TestTeamNull(unittest.TestCase):
     def test_aggie_pride(self):
         """Test to make sure the Aggie Pride function works"""
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
+
+    def test_au_medicare(self):
+        """Test to make sure AU_MEDICARE works"""
+        # https://stackoverflow.com/questions/3589345/how-do-i-validate-an-australian-medicare-number
+        results = analyze_text('2123 45670 1')
+        print(results)
+        self.assertIn('AU_MEDICARE', str(results))
