872bfb57e7b18c711133c6c05da9d854a82fc322
Jaquan jackson <113326779+KidSens@users.noreply.github.com>
2023-10-16 00:51:56 +0000

    Revert "git commit -m 'message'"
    This reverts commit c22ec8b3d14362dec8812f4fe40d8a6ae74f8acc.

--- test_team_sci.py
+++ test_team_sci.py
@@ -1,30 +1,8 @@
 import unittest
-from pii_scan import analyze_text, show_aggie_pride
+from pii_scan import show_aggie_pride, analyze_text
+
 
 class TestTeamSci(unittest.TestCase):
     def test_aggie_pride(self):
         """Test to make sure the Aggie Pride function works"""
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
-
-    def test_itin_detect(self):
-        """Test to make sure ITIN (US_ITIN) is detected"""
-
-        # Positive Testcase with a valid ITIN
-        results = analyze_text('My ITIN is 123-45-6789')
-        print(results)
-        self.assertIn('US_ITIN', str(results))
-
-        # Positive Testcase with another valid ITIN
-        results = analyze_text('My ITIN is 456-78-9012')
-        print(results)
-        self.assertIn('US_ITIN', str(results))
-
-        # Negative Testcase with a string that does not contain an ITIN
-        results = analyze_text('My phone number is 789-01-2345')
-        print(results)
-        self.assertNotIn('US_ITIN', str(results))
-
-        # Negative Testcase with a string that does not contain an ITIN
-        results = analyze_text('My ITIN is 124143536')
-        print(results)
-        self.assertNotIn('US_ITIN', str(results))
