222664be7349862deadde3ef0a7983764cbc17a3
Tobi <53277733+tfortobi@users.noreply.github.com>
2023-10-16 16:45:15 +0000

    Add test case for philisophical belief detection

--- test_team_code_busters.py
+++ test_team_code_busters.py
@@ -6,3 +6,14 @@ class TestTeamCodeBusters(unittest.TestCase):
     def test_aggie_pride(self):
         """Test to make sure the Aggie Pride function works"""
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
+
+    def test_philisophical_belief_detect(self):
+        #positive test case
+        results = analyze_text("I am a secularist")
+        print(results)
+        self.assertIn('',str(results))
+
+        #negative test case
+        results = analyze_text("I am atheist")
+        print(results)
+        self.assertIn('',str(results))
