79d28c23ce3bf1c67af6acedea6f1de8f7b85138
Lesterconey <69806358+Lesterconey@users.noreply.github.com>
2023-10-16 22:57:43 +0000

    Adding test cases for detecting eye color.

--- test_team_code_busters.py
+++ test_team_code_busters.py
@@ -6,3 +6,16 @@ class TestTeamCodeBusters(unittest.TestCase):
     def test_aggie_pride(self):
         """Test to make sure the Aggie Pride function works"""
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
+
+    def test_eye_color_detect(self):
+        '''Testing if eye color is detected'''
+
+        #positive test case
+        results = analyze_text('Eye color: Red')
+        print(results)
+        self.assertIn('', str(results))
+
+        #negative test case
+        results = analyze_text('Eye color: Ball')
+        print(results)
+        self.assertIn('', str(results))
