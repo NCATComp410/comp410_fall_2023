18bdffea345877197a6598aefac74e9602faeda3
tolanism <71404187+tolanism@users.noreply.github.com>
2023-10-17 02:57:27 +0000

    Detect birthdates with DATE_TIME entity

--- test_team_code_busters.py
+++ test_team_code_busters.py
@@ -3,10 +3,7 @@ from pii_scan import show_aggie_pride, analyze_text
 
 
 class TestTeamCodeBusters(unittest.TestCase):
-    def test_aggie_pride(self):
-        """Test to make sure the Aggie Pride function works"""
-        self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
-
+      
     def test_birthdate_detect(self):
         #Positive test case
 
