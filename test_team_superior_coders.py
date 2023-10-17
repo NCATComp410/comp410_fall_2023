402ad5b3924e556397447d931eed8e8493f73fb0
Kevin Smoot III <53538181+KevSmoot19@users.noreply.github.com>
2023-10-17 11:17:21 +0000

    Implemented IP Address Detection

--- test_team_superior_coders.py
+++ test_team_superior_coders.py
@@ -6,3 +6,16 @@ class TestSuperiorCoders(unittest.TestCase):
     def test_aggie_pride(self):
         """Test to make sure the Aggie Pride function works"""
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
+
+    def test_ipv4_address(self):
+        """Test to make sure ipv4 address id detected"""
+
+        # positive test case
+        results = analyze_text('My IP address is 123.123.123.123')
+        print(results)
+        self.assertIn('IP_ADDRESS', str(results))
+
+        # negative test case
+        results = analyze_text('My IP address is 123.123.23')
+        print(results)
+        self.assertNotIn('IP_ADDRESS', str(results))
