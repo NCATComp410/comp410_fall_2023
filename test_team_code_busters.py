f9b89a2652d2ba3bdd3a42941b3cbca7625fb627
tolanism <71404187+tolanism@users.noreply.github.com>
2023-10-17 00:44:38 +0000

    Implemeneted Birth date Detection

--- test_team_code_busters.py
+++ test_team_code_busters.py
@@ -6,3 +6,20 @@ class TestTeamCodeBusters(unittest.TestCase):
     def test_aggie_pride(self):
         """Test to make sure the Aggie Pride function works"""
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
+
+
+def test_aggie_pride(self):
+    """Test to make sure Aggie Pride function is working """
+    self.assertEqual('Aggie Pride !',show_aggie_pride())
+
+def test_birthdate_detect(self):
+    #Positive test case
+
+    results = analyze_text(" My birthdate: 11/01/2002")
+    print(results)
+    self.assertIn('', str(results))
+
+    #Negative test case
+    results = analyze_text(" My birthdate: ABC/de/frog.")
+    print(results)
+    self.assertNotIn('', str(results))
