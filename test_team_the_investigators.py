1c4311874159ffab6fc5f55994967efae13b6974
mjonesncat <143550275+mjonesncat@users.noreply.github.com>
2023-10-17 15:46:17 +0000

    Implemented full name detection

--- test_team_the_investigators.py
+++ test_team_the_investigators.py
@@ -6,3 +6,15 @@ class TestTheInvestigators(unittest.TestCase):
     def test_aggie_pride(self):
         """Test to make sure the Aggie Pride function works"""
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
+
+def test_full_names_detect(self):
+     """Test to make sure full names are detected"""
+    # Positive test case
+     results = analyze_text('John William Smith')
+     print(results)
+     self.assertIn('PERSON', str(results))
+
+    # Negative test case
+     results = analyze_text('John@123')
+     print(results)
+     self.assertNotIn('PERSON', str(results))
