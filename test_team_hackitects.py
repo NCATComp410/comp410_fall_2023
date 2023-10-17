7c451f05f692c667fd601ac300db54bbfb7020d8
Jennifer Metume-Ibe <89212067+jenniferibe@users.noreply.github.com>
2023-10-17 09:32:05 -0400

    test_team_hackitects.py

--- test_team_hackitects.py
+++ test_team_hackitects.py
@@ -6,21 +6,3 @@ class TestTeamHackitects(unittest.TestCase):
     def test_aggie_pride(self):
         """Test to make sure the Aggie Pride function works"""
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
-
-    def test_marital_status_detect(self):
-        """Testing for Marital Status"""
-        
-        # Positive Test Cases - 1
-        results = analyze_text('Marital Status - Married')
-        print(results)
-        self.assertIn('MARITAL_STATUS', str(results))
-        
-        # Positive Test Cases - 2
-        results = analyze_text('Marital Status - Divorced')
-        print(results)
-        self.assertIn('MARITAL_STATUS', str(results))
-
-        # Negative Test Case
-        results = analyze_text('Marital Status - M@rried!')
-        print(results)
-        self.assertNotIn('MARITAL_STATUS', str(results))
