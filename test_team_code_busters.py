eb37ec281fb61d87eb7d16896b4c5896380d5642
jewelworks <143753121+jewelworks@users.noreply.github.com>
2023-10-17 19:10:34 +0000

    Implemented Health Insurance Policy Number

--- test_team_code_busters.py
+++ test_team_code_busters.py
@@ -6,3 +6,23 @@ class TestTeamCodeBusters(unittest.TestCase):
     def test_aggie_pride(self):
         """Test to make sure the Aggie Pride function works"""
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
+
+    def test_health_insurance_policy_number_detect(self):
+        """Test to ensure policy number is detected"""
+        #positive testcase
+        """Insurance Policy number should begin with 3 letters followed by 9 numbers"""
+        results = analyze_text('My insurance policy number is XYZ123456789')
+        print(results)
+        self.assertIn('MEDICAL_LICENSE', str(results))
+
+        #negative testcase
+        results = analyze_text('My insurance policy number is AB1234567890')
+        """This policy ID is incorrect because it has 2 Letters and 10 Numbers"""
+        print(results)
+        self.assertNotIn('MEDICAL_LICENSE', str(results))
+
+        #negative testcase
+        results = analyze_text('My insurance policy number is ABCD12345678')
+        """This policy ID is incorrect because it contains 4 Letters and 8 Numbers"""
+        print(results)
+        self.assertNotIn('MEDICAL_LICENSE', str(results))
