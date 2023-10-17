31c0a7e9882b96de8d0c10cd35c38ce581080c18
Vgoods <88050046+Vgoods@users.noreply.github.com>
2023-10-17 00:58:18 +0000

    Implementation of Amex Credit Card detection

--- test_team_the_investigators.py
+++ test_team_the_investigators.py
@@ -13,19 +13,19 @@ class TestTheInvestigators(unittest.TestCase):
         #Postitive Case
         results = analyze_text('My Amex credt card number is 3711 465932 57302')
         print(results)
-        self.assertIn('AMEX', str(results))
+        self.assertIn('CREDIT_CARD', str(results))
 
         #Negative Case
         results = analyze_text('My Amex card number is 6011 473832 85948')
         print(results)
-        self.assertIn('AMEX', str(results))
+        self.assertIn('CREDIT_CARD', str(results))
 
         #Postitive Case
         results = analyze_text('My Amex card number is 3403 129054 39204')
         print(results)
-        self.assertIn('AMEX', str(results))
+        self.assertIn('CREDIT_CARD', str(results))
 
         #Negative Case
         results = analyze_text('My Amex card number is 3011 4738 3277 8594')
         print(results)
-        self.assertIn('AMEX', str(results))
+        self.assertIn('CREDIT_CARD', str(results))
