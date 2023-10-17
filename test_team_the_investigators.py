cb2273aafcf2ae46eaae148f22b30060a640610e
Vgoods <88050046+Vgoods@users.noreply.github.com>
2023-10-17 01:28:00 +0000

    Changes made for implementation of Amex detection

--- test_team_the_investigators.py
+++ test_team_the_investigators.py
@@ -3,29 +3,16 @@ from pii_scan import show_aggie_pride, analyze_text
 
 
 class TestTheInvestigators(unittest.TestCase):
-   # def test_aggie_pride(self):
-     #   """Test to make sure the Aggie Pride function works"""
-      #  self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
+    
+    def test_credit_card_detect(self):
+        """Test to see if Amex credit card is detected"""
 
-    def test_amex_card_detect(self):
-        """Test to make sure the Amex Card is detected"""
-
-        #Postitive Case
-        results = analyze_text('My Amex credt card number is 3711 465932 57302')
+        # Positive Testcase
+        results = analyze_text('My Amex credit card number is 3400 000000 00009')
         print(results)
         self.assertIn('CREDIT_CARD', str(results))
 
-        #Negative Case
-        results = analyze_text('My Amex card number is 6011 473832 85948')
+        # Positive Testcase
+        results = analyze_text('My Amex credit card number is 4400 000000 00009')
         print(results)
-        self.assertIn('CREDIT_CARD', str(results))
-
-        #Postitive Case
-        results = analyze_text('My Amex card number is 3403 129054 39204')
-        print(results)
-        self.assertIn('CREDIT_CARD', str(results))
-
-        #Negative Case
-        results = analyze_text('My Amex card number is 3011 4738 3277 8594')
-        print(results)
-        self.assertIn('CREDIT_CARD', str(results))
+        self.assertNotIn('CREDIT_CARD', str(results))
