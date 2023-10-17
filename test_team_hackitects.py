32fe5f894910cad3f6fa714902221e5586d79755
claesmk <claesmk@gmail.com>
2023-10-16 20:29:04 -0400

    Fixed issues with import and indentations

--- test_team_hackitects.py
+++ test_team_hackitects.py
@@ -3,30 +3,22 @@ from pii_scan import show_aggie_pride, analyze_text
 
 
 class TestTeamHackitects(unittest.TestCase):
+
     def test_aggie_pride(self):
         """Test to make sure the Aggie Pride function works"""
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
-    import unittest
-from pii_scan import show_aggie_pride, analyze_text
 
+    def test_addressDew(self):
+        """Location testing"""
+        #Uses both Custom Dewberry regex is DewLocEnt and built in location recognizer
 
-class TestTeamHackitects(unittest.TestCase):
-    
-     def test_aggie_pride(self):
-        """Test to make sure the Aggie Pride function works"""
-        self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
-
-     def test_addressDew(self):
-        "Location testing"
-        #Uses both Custom Dewberry regex is DewLocEnt and built in location recognizer 
-
-        #two positive test cases. 
+        #two positive test cases.
         #NCAT address.
         res = analyze_text('1601 E Market St, Greensboro, NC 27411')
         self.assertIn('LOCATION', str(res))
         self.assertIn('DewLocEnt', str(res))
 
-        #UNCG address. 
+        #UNCG address.
         res = analyze_text('1400 Spring Garden St, Greensboro, NC 27412')
         self.assertIn('LOCATION', str(res))
         self.assertIn('DewLocEnt', str(res))
@@ -37,6 +29,5 @@ class TestTeamHackitects(unittest.TestCase):
         res = analyze_text('Brazil in the 1900s')
         #this assertIn statment SHOULD fail
         self.assertIn('LOCATION', str(res))
-        #default fails, mine works. 
+        #default fails, mine works.
         self.assertNotIn('DewLoCEnt', str(res))
-        
