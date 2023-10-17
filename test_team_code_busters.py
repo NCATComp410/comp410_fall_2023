414f91ff0cdc195c90e8ffe009f8719001831c87
claesmk <claesmk@gmail.com>
2023-10-16 22:07:04 -0400

    Corrected indentation and DATE_TIME entity

--- test_team_code_busters.py
+++ test_team_code_busters.py
@@ -7,19 +7,14 @@ class TestTeamCodeBusters(unittest.TestCase):
         """Test to make sure the Aggie Pride function works"""
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
 
+    def test_birthdate_detect(self):
+        #Positive test case
 
-def test_aggie_pride(self):
-    """Test to make sure Aggie Pride function is working """
-    self.assertEqual('Aggie Pride !',show_aggie_pride())
+        results = analyze_text(" My birthdate: 11/01/2002")
+        print(results)
+        self.assertIn('DATE_TIME', str(results))
 
-def test_birthdate_detect(self):
-    #Positive test case
-
-    results = analyze_text(" My birthdate: 11/01/2002")
-    print(results)
-    self.assertIn('', str(results))
-
-    #Negative test case
-    results = analyze_text(" My birthdate: ABC/de/frog.")
-    print(results)
-    self.assertNotIn('', str(results))
+        #Negative test case
+        results = analyze_text(" My birthdate: ABC/de/frog.")
+        print(results)
+        self.assertNotIn('DATE_TIME', str(results))
