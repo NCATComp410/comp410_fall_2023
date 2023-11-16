59d8dfa73454f86dd7f28948008a9bc4538762fe
claesmk <44329220+claesmk@users.noreply.github.com>
2023-11-16 02:05:50 +0000

    Added sprint2 fixes

--- pii_scan.py
+++ pii_scan.py
@@ -287,7 +287,7 @@ def scan_files(start_path):
                           'IP_ADDRESS', 'AU_MEDICARE', 'US_PASSPORT', 'UUID', 'INTERNATIONAL_PN', 'PERSON', 'BIRTHDATE',
                           'POB', 'NPR', 'US_BANK_NUMBER', 'EYE_COLOR', 'UDID', 'INTEREST', 'GENDER',
                           'CRYPTO', 'MARITALSTATS', 'LOCATION', 'US_SSN', 'US_ITIN', 'MAC_ADDRESS', 'STUDENT_ID',
-                          'RACE', 'USERNAME', 'CREDIT_SCORE', 'PHONE_NUMBER']
+                          'RACE', 'USERNAME', 'CREDIT_SCORE', 'PHONE_NUMBER', 'NCDL']
 
     # check to make sure start_path is a directory
     if not os.path.isdir(start_path):
